ff3fb3dcdaf231ee2d286e07a9c4393fe2792e01
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-11-10 22:44:16 +0700

    add order model

---
+++ SerializerComparison/SerializerComparison/Order.cs
@@ -0,0 +1,47 @@
+﻿namespace SerializerComparison;
+
+public class Order
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; }
+
+    public string Category { get; set; }
+
+    public int TotalAmount { get; set; }
+
+    public List<Item> Items { get; set; }
+
+    public Order Create()
+    {
+        return new Order
+        {
+            Name = "Book Order",
+            Category = "Books",
+
+            TotalAmount = 100,
+            Items = new List<Item>()
+            {
+                new()
+                {
+                    Name = "Item 1",
+                    Amount = 50
+                },
+                new()
+                {
+                    Name = "Item 2",
+                    Amount = 50
+                }
+            }
+        };
+    }
+}
+
+public class Item
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; }
+
+    public int Amount { get; set; }
+}
