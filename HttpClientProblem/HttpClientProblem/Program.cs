a6fffa4ea7cfe82cd09d3807abcb18316e8a868a
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-01-02 18:32:40 +0700

    cleanup

--- HttpClientProblem/HttpClientProblem/Program.cs
+++ HttpClientProblem/HttpClientProblem/Program.cs
@@ -1,13 +1,11 @@
 ﻿
 Console.WriteLine("Start connections...");
+var client = new HttpClient();
 
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < 1000; i++)
 {
-    using (var client = new HttpClient())
-    {
-        var result = await client.GetAsync("http://google.com");
-        Console.WriteLine($"Request sent with status: {result.StatusCode}");
-    }
+    var result = await client.GetAsync("http://google.com");
+    Console.WriteLine($"Request sent with status: {result.StatusCode}");
 }
 
 Console.WriteLine("Stop connections...");
