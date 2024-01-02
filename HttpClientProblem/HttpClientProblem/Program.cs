f96b1484a96465a98d202f1a5ab9d51a0055f87e
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-01-02 17:45:20 +0700

    add http client code

--- HttpClientProblem/HttpClientProblem/Program.cs
+++ HttpClientProblem/HttpClientProblem/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+Console.WriteLine("Start connections...");
+
+for (int i = 0; i < 10; i++)
+{
+    using (var client = new HttpClient())
+    {
+        var result = await client.GetAsync("http://google.com");
+        Console.WriteLine($"Request sent with status: {result.StatusCode}");
+    }
+}
+
+Console.WriteLine("Stop connections...");
