513ac2633d1a3b5e70afc87ec76308c2ad18a932
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-02-02 22:26:00 +0700

    add cache provider

---
+++ MutliLayerCache/MutliLayerCache/CacheProviders/ICacheProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace MutliLayerCache.CacheProviders;
+
+public interface ICacheProvider
+{
+    Task<T> GetOrAddAsync<T>(string key, Func<Task<T>> getFromDbFunction, TimeSpan expiry, CancellationToken cancellationToken);
+
+    Task<bool> DeleteAsync(string key, CancellationToken cancellationToken);
+}
