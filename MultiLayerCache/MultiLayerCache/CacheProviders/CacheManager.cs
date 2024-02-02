c89a6adb9bbaaf4a2a448f9b725400419085784b
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-02-02 23:57:58 +0700

    add CacheManager

---
+++ MultiLayerCache/MultiLayerCache/CacheProviders/CacheManager.cs
@@ -0,0 +1,44 @@
+﻿namespace MultiLayerCache.CacheProviders;
+
+public class CacheManager(ICacheProvider[] cacheProviders, ILogger<CacheManager> logger)
+{
+    public async Task<T> GetOrAddAsync<T>(string key, Func<Task<T>> getFromDbFunction, TimeSpan expiry)
+    {
+        logger.LogInformation("Try to get value from cache: {key}", key);
+
+        foreach (var cacheProvider in cacheProviders)
+        {
+            var cachedValue = await cacheProvider.GetAsync<T>(key);
+
+            if (cachedValue != null)
+            {
+                logger.LogInformation("=====> HIT cache for {key} in {cacheProvider}", key, cacheProvider.GetType().Name);
+
+                return cachedValue;
+            }
+            else
+                logger.LogInformation("Cache wasn't hit for {key} in {cacheProvider}", key, cacheProvider.GetType().Name);
+        }
+
+        logger.LogInformation("Not found in cache: {key}", key);
+
+        var result = await getFromDbFunction();
+
+        for (int i = 0; i < cacheProviders.Length; i++)
+        {
+            var expirySeconds = (i * 2 == 0 ? 1 : i * 2) * expiry.TotalSeconds;
+
+            await cacheProviders[i].SaveAsync(key, result, TimeSpan.FromSeconds(expirySeconds));
+        }
+
+        return result;
+    }
+
+    public async Task DeleteAsync(string key)
+    {
+        foreach (var cacheProvider in cacheProviders)
+        {
+            await cacheProvider.DeleteAsync(key);
+        }
+    }
+}
--- MultiLayerCache/MultiLayerCache/CacheProviders/InMemoryCacheProvider.cs
+++ MultiLayerCache/MultiLayerCache/CacheProviders/InMemoryCacheProvider.cs
@@ -2,53 +2,18 @@
 using Microsoft.Extensions.Caching.Memory;
 namespace MultiLayerCache.CacheProviders;
 
-public class InMemoryCacheProvider : ICacheProvider
+public class InMemoryCacheProvider(IMemoryCache memCache, ILogger<InMemoryCacheProvider> logger) : ICacheProvider
 {
-    private readonly IMemoryCache _memCache;
-    private readonly ILogger<InMemoryCacheProvider> _logger;
-
-    public InMemoryCacheProvider(IMemoryCache memCache,
-        ILogger<InMemoryCacheProvider> logger)
-    {
-        _memCache = memCache;
-        _logger = logger;
-    }
-
-    public async Task<T> GetOrAddAsync<T>(string key, Func<Task<T>> getFromDbFunction,
-        TimeSpan expiry, CancellationToken cancellationToken)
-    {
-        _logger.LogInformation("Try to get value from in-memory cache: {key}", key);
-
-        if (_memCache.TryGetValue(key, out T? cacheResult))
-        {
-            _logger.LogInformation("=====> HIT in-memory cache: {key}", key);
-            return cacheResult!;
-        }
-
-        _logger.LogInformation("Cache not found for in-memory cache: {key}", key);
-
-        var result = await getFromDbFunction();
-
-        var options = new MemoryCacheEntryOptions
-        {
-            AbsoluteExpirationRelativeToNow = expiry
-        };
-
-        _memCache.Set(key, result, options);
-
-        return result;
-    }
-
     public Task<bool> DeleteAsync(string key)
     {
         try
         {
-            _memCache.Remove(key);
+            memCache.Remove(key);
             return Task.FromResult(true);
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Error on delete cache key: {key}", key);
+            logger.LogError(ex, "Error on delete cache key: {key}", key);
             return Task.FromResult(false);
         }
     }
@@ -60,17 +25,17 @@ public class InMemoryCacheProvider : ICacheProvider
             AbsoluteExpirationRelativeToNow = expiry
         };
 
-        _memCache.Set(key, value, options);
+        memCache.Set(key, value, options);
 
-        _logger.LogInformation("Data saved to in-memory cache, key: {key} expiry:{expiry}", key, expiry.TotalSeconds);
+        logger.LogInformation("Data saved to in-memory cache, key: {key} expiry:{expiry}", key, expiry.TotalSeconds);
 
         return Task.CompletedTask;
     }
 
     public Task<T?> GetAsync<T>(string key)
     {
-        if (_memCache.TryGetValue(key, out T? cacheResult))
-            _logger.LogInformation("=====> HIT in-memory cache: {key}", key);
+        if (memCache.TryGetValue(key, out T? cacheResult))
+            logger.LogInformation("=====> HIT in-memory cache: {key}", key);
 
         return Task.FromResult(cacheResult);
     }
