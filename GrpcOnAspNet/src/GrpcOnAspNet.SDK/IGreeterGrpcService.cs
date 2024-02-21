4c631c2d0e3a39444d8db33f3a5afb5991d3097c
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-02-21 12:55:11 +0700

    add grpc on net project

---
+++ GrpcOnAspNet/src/GrpcOnAspNet.SDK/IGreeterGrpcService.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using GrpcOnAspNet.Server;
+
+namespace GrpcOnAspNet.SDK;
+
+public interface IGreeterGrpcService
+{
+    Task<string> SayHelloAsync(string name, CancellationToken cancellationToken);
+}
+
+
+public class GreeterGrpcService : IGreeterGrpcService
+{
+    private readonly Greeter.GreeterClient grpcClient;
+
+    public GreeterGrpcService(Greeter.GreeterClient grpcClient)
+    {
+        this.grpcClient = grpcClient;
+    }
+
+    public async Task<string> SayHelloAsync(string name, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await grpcClient.SayHelloAsync(new HelloRequest { Name = name }, cancellationToken: cancellationToken);
+
+            return result.Message;
+        }
+        catch (RpcException ex)
+        {
+            throw;
+        }
+
+    }
+}
---
+++ GrpcOnAspNet/src/GrpcOnAspNet.SDK/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using GrpcOnAspNet.Server;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GrpcOnAspNet.SDK;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddGrpcSdk(this IServiceCollection services)
+    {
+        services.AddGrpcClient<Greeter.GreeterClient>(client =>
+        {
+            client.Address = new Uri("https://localhost:7251");
+        });
+
+        services.AddScoped<IGreeterGrpcService, GreeterGrpcService>();
+    }
+}
