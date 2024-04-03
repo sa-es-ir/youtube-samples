f97707b02b32a65b0a7a42e28e5674dac1db0409
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-04-03 18:54:11 +0700

    feature: add CustomMediatR

--- MultiTenantDbContext/MultiTenantDbContext/TenantProvider.cs
+++ MultiTenantDbContext/MultiTenantDbContext/TenantProvider.cs
@@ -2,20 +2,7 @@
 
 public sealed class TenantProvider
 {
-    private readonly IHttpContextAccessor httpContextAccessor;
-
-    public TenantProvider(IHttpContextAccessor httpContextAccessor)
-    {
-        this.httpContextAccessor = httpContextAccessor;
-    }
-
+   
     public string? TenantId { get; set; }
-
-    public void GetTenant()
-    {
-        TenantId = httpContextAccessor
-            .HttpContext
-            .Request
-            .Query[""];
-    }
+    
 }
