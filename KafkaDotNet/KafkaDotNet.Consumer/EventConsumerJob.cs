4f627c7be8d509e47785949070adc9764f089b9a
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-05-15 22:45:22 +0700

    Update EventConsumerJob.cs

--- KafkaDotNet/KafkaDotNet.Consumer/EventConsumerJob.cs
+++ KafkaDotNet/KafkaDotNet.Consumer/EventConsumerJob.cs
@@ -39,7 +39,7 @@ internal class EventConsumerJob : BackgroundService
 
                 _logger.LogInformation($"Consumed message '{consumeResult.Message.Value}' at: '{consumeResult.Offset}'");
             }
-            catch (OperationCanceledException)
+            catch (Exception)
             {
                 // Ignore
             }
