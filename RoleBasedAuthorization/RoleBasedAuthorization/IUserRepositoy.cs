8fa8c534f7912d54c9659f1edf53130de5e7b11d
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-05-22 17:39:54 +0700

    add IUserRepository

---
+++ RoleBasedAuthorization/RoleBasedAuthorization/IUserRepositoy.cs
@@ -0,0 +1,14 @@
+﻿namespace RoleBasedAuthorization;
+
+public interface IUserRepositoy
+{
+    Task<List<string>> GetUserRolesAsync(string? username, CancellationToken cancellationToken);
+}
+
+public class UserRepository : IUserRepositoy
+{
+    public Task<List<string>> GetUserRolesAsync(string? username, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new List<string> { "Admin" });
+    }
+}
--- RoleBasedAuthorization/RoleBasedAuthorization/Program.cs
+++ RoleBasedAuthorization/RoleBasedAuthorization/Program.cs
@@ -1,9 +1,11 @@
+using RoleBasedAuthorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserRepositoy, UserRepository>();
+
 
 var app = builder.Build();
 
