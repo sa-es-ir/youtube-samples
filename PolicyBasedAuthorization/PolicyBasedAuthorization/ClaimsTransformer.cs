29f2f24e4f5707f494f514c4b7a5e308caf161ea
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-06-05 22:19:23 +0700

    add ClaimsTransfer

---
+++ PolicyBasedAuthorization/PolicyBasedAuthorization/ClaimsTransformer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using PolicyBasedAuthorization;
+using System.Security.Claims;
+
+internal class ClaimsTransformer(UserService userService) : IClaimsTransformation
+{
+    public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+    {
+        var userClaims = userService.GetUserClaims(principal.Identity!.Name!);
+
+        var identity = new ClaimsIdentity(userClaims.Select(claim => new Claim("user-group", claim)));
+
+        principal.AddIdentity(identity);
+
+        return Task.FromResult(principal);
+    }
+}
