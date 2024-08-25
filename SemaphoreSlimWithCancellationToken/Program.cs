d05caaf4c848f6cf1bf7d12ba4e79f78772c08ab
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-08-25 23:02:15 +0700

    call sempaphore method

--- SemaphoreSlimWithCancellationToken/Program.cs
+++ SemaphoreSlimWithCancellationToken/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SemaphoreSlimWithCancellationToken;
+
 Console.WriteLine("Hello, World!");
+
+
+var fooService = new FooService();
+
+var cts = new CancellationTokenSource();
+
+var task1 = fooService.DoSomethingAsync(cts.Token);
+var task2 = fooService.DoSomethingAsync(cts.Token);
+var task3 = fooService.DoSomethingAsync(cts.Token);
+var task4 = fooService.DoSomethingAsync(cts.Token);
+var task5 = fooService.DoSomethingAsync(cts.Token);
+
+await Task.WhenAll(task1, task2, task3, task4, task5);
