691342d0df91cc1c5dd2ffc0fa86eb729ed2b11c
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-08-25 23:02:28 +0700

    add foo service

---
+++ SemaphoreSlimWithCancellationToken/FooService.cs
@@ -0,0 +1,21 @@
+﻿namespace SemaphoreSlimWithCancellationToken;
+
+public class FooService
+{
+    private readonly SemaphoreSlim _semaphore = new(initialCount: 1);
+
+    public async Task DoSomethingAsync(CancellationToken cancellationToken)
+    {
+        await _semaphore.WaitAsync();
+        try
+        {
+            Console.WriteLine($"Enter to Critical zone at {TimeProvider.System.GetLocalNow()}");
+
+            await Task.Delay(1000, cancellationToken);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
