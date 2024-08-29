9a837e35b4f91cbe5aee5521917f47a2ef850540
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-08-29 22:22:11 +0700

    add semaphore with ct

---
+++ SemaphoreSlimWithCancellationToken/SemaphoreWithCT.cs
@@ -0,0 +1,21 @@
+﻿namespace SemaphoreSlimWithCancellationToken;
+
+public class SemaphoreWithCT
+{
+    private readonly SemaphoreSlim _semaphore = new(initialCount: 1);
+
+    public async Task DoSomethingAsync(CancellationToken cancellationToken)
+    {
+        await _semaphore.WaitAsync(TimeSpan.FromMilliseconds(500));
+        try
+        {
+            Console.WriteLine($"Enter to Critical zone at {TimeProvider.System.GetLocalNow()}");
+
+            await Task.Delay(1000);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
