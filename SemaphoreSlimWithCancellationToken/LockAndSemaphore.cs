a3ebdc98a23646acb5f2d84dde78cc8980ab8610
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-08-29 22:21:47 +0700

    add lock and semaphore

---
+++ SemaphoreSlimWithCancellationToken/LockAndSemaphore.cs
@@ -0,0 +1,33 @@
+﻿namespace SemaphoreSlimWithCancellationToken;
+
+internal class LockAndSemaphore
+{
+    private readonly SemaphoreSlim _semaphore = new(initialCount: 2);
+
+    public async Task DoWithSemaphore(int taskNumber)
+    {
+        await _semaphore.WaitAsync();
+        try
+        {
+            Console.WriteLine($"{taskNumber} Enter to Critical zone at {TimeProvider.System.GetLocalNow()}");
+
+            await Task.Delay(1000);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public async Task DoWithLock(int taskNumber)
+    {
+        lock (_semaphore)
+        {
+            Console.WriteLine($"{taskNumber} Enter to Critical zone at {TimeProvider.System.GetLocalNow()}");
+
+            Task.Delay(1000).Wait();
+        }
+
+        await Task.CompletedTask;
+    }
+}
