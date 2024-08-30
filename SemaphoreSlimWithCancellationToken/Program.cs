a8d46eb1c34699c5ee06ae24cc9782a697e7da6c
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-08-30 09:56:20 +0700

    change logs

--- SemaphoreSlimWithCancellationToken/Program.cs
+++ SemaphoreSlimWithCancellationToken/Program.cs
@@ -3,6 +3,8 @@
 
 var semaphore = new LockAndSemaphore();
 
+Console.WriteLine("---SemaphoreSlim---");
+
 var task1s = semaphore.DoWithSemaphore(1);
 var task2s = semaphore.DoWithSemaphore(2);
 var task3s = semaphore.DoWithSemaphore(3);
@@ -10,17 +12,13 @@ var task4s = semaphore.DoWithSemaphore(4);
 
 await Task.WhenAll(task1s, task2s, task3s, task4s);
 
+
+Console.WriteLine("---Lock---");
 var task1l = semaphore.DoWithLock(1);
 var task2l = semaphore.DoWithLock(2);
 var task3l = semaphore.DoWithLock(3);
 var task4l = semaphore.DoWithLock(4);
 
+
 await Task.WhenAll(task1l, task2l, task3l, task4l);
 
-
-var semaphoreWithCT = new SemaphoreWithCT();
-
-var cts = new CancellationTokenSource();
-cts.CancelAfter(500);
-
-await semaphoreWithCT.DoSomethingAsync(cts.Token);
