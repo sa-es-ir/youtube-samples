c270522d4d3708da702500455cb27768ce1a151e
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-08-30 09:56:36 +0700

    add semaphore versions

--- SemaphoreSlimWithCancellationToken/SemaphoreV1.cs
+++ SemaphoreSlimWithCancellationToken/SemaphoreV1.cs
@@ -1,17 +1,22 @@
 ﻿namespace SemaphoreSlimWithCancellationToken;
 
-public class SemaphoreWithCT
+public class SemaphoreV1
 {
-    private readonly SemaphoreSlim _semaphore = new(initialCount: 1);
+    private readonly SemaphoreSlim _semaphore = new(initialCount: 1, maxCount: 1);
 
     public async Task DoSomethingAsync(CancellationToken cancellationToken)
     {
-        await _semaphore.WaitAsync(TimeSpan.FromMilliseconds(500));
+        await _semaphore.WaitAsync(cancellationToken);
         try
         {
             Console.WriteLine($"Enter to Critical zone at {TimeProvider.System.GetLocalNow()}");
 
-            await Task.Delay(1000);
+            await Task.Delay(1000, cancellationToken);
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"-->ERR: {ex.Message}");
         }
         finally
         {
---
+++ SemaphoreSlimWithCancellationToken/SemaphoreV2.cs
@@ -0,0 +1,29 @@
+﻿namespace SemaphoreSlimWithCancellationToken;
+
+public class SemaphoreV2
+{
+    private readonly SemaphoreSlim _semaphore = new(initialCount: 1, maxCount: 1);
+
+    public async Task DoSomethingAsync(CancellationToken cancellationToken)
+    {
+        var lockTaken = false;
+        try
+        {
+            await _semaphore.WaitAsync(cancellationToken);
+            Console.WriteLine($"Enter to Critical zone at {TimeProvider.System.GetLocalNow()}");
+
+            await Task.Delay(1000, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"-->ERR: {ex.Message}");
+        }
+        finally
+        {
+            if (lockTaken)
+            {
+                _semaphore.Release();
+            }
+        }
+    }
+}
