4a22bc828f18cb714ef28eb4f05037119d2c3b90
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-09-01 11:25:59 +0700

    calling semaphore methods

--- SemaphoreSlimWithCancellationToken/Program.cs
+++ SemaphoreSlimWithCancellationToken/Program.cs
@@ -1,24 +1,34 @@
 ﻿using SemaphoreSlimWithCancellationToken;
 
 
+
+var semaphoreV1 = new SemaphoreV2();
+
+var cts = new CancellationTokenSource();
+cts.CancelAfter(500);
+
+await semaphoreV1.DoSomethingAsync(cts.Token);
+
+await semaphoreV1.DoSomethingAsync(CancellationToken.None);
+
+
 var semaphore = new LockAndSemaphore();
 
-Console.WriteLine("---SemaphoreSlim---");
+//Console.WriteLine("---SemaphoreSlim---");
 
-var task1s = semaphore.DoWithSemaphore(1);
-var task2s = semaphore.DoWithSemaphore(2);
-var task3s = semaphore.DoWithSemaphore(3);
-var task4s = semaphore.DoWithSemaphore(4);
+//var task1s = semaphore.DoWithSemaphore(1);
+//var task2s = semaphore.DoWithSemaphore(2);
+//var task3s = semaphore.DoWithSemaphore(3);
+//var task4s = semaphore.DoWithSemaphore(4);
 
-await Task.WhenAll(task1s, task2s, task3s, task4s);
+//await Task.WhenAll(task1s, task2s, task3s, task4s);
+
+//Console.WriteLine("---Lock---");
+//var task1l = semaphore.DoWithLock(1);
+//var task2l = semaphore.DoWithLock(2);
+//var task3l = semaphore.DoWithLock(3);
+//var task4l = semaphore.DoWithLock(4);
 
 
-Console.WriteLine("---Lock---");
-var task1l = semaphore.DoWithLock(1);
-var task2l = semaphore.DoWithLock(2);
-var task3l = semaphore.DoWithLock(3);
-var task4l = semaphore.DoWithLock(4);
-
-
-await Task.WhenAll(task1l, task2l, task3l, task4l);
+//await Task.WhenAll(task1l, task2l, task3l, task4l);
 
