2fa85c7b3e4935745e1e13bc6bb1d8e2a344d395
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-09-07 13:00:10 +0700

    add entity models

---
+++ EFCoreLargeData/Entities/TextTable1MB.cs
@@ -0,0 +1,15 @@
+﻿namespace EFCoreLargeData.Entities;
+
+public class TextTable1MB
+{
+    public int Id { get; set; }
+
+    public string Text { get; set; } = string.Empty;
+}
+
+public class TextTable2MB
+{
+    public int Id { get; set; }
+
+    public string Text { get; set; } = string.Empty;
+}
