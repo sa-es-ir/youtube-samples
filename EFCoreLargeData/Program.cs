653897467f6db471bfd181db70fb03a12440e1c3
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-09-08 13:11:59 +0700

    cleanup

--- EFCoreLargeData/Program.cs
+++ EFCoreLargeData/Program.cs
@@ -1,16 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 using EFCoreLargeData;
 
-Console.WriteLine("EF Large Data");
+Console.WriteLine("----EF Large Data Benchmark------");
 
 BenchmarkRunner.Run<EFBenchmark>();
-
-//var dbcontext = new TestDbContext();
-
-//dbcontext.TextTable1MBs.Add(new TextTable1MB { Text = new string('x', 1024 * 1024 * 1) });
-//dbcontext.TextTable2MBs.Add(new TextTable2MB { Text = new string('x', 1024 * 1024 * 2) });
-
-//await dbcontext.SaveChangesAsync();
-
-//await dbcontext.TextTables.ToListAsync();
--- EFCoreLargeData/TestDbContext.cs
+++ EFCoreLargeData/TestDbContext.cs
@@ -5,12 +5,18 @@ namespace EFCoreLargeData;
 
 public class TestDbContext : DbContext
 {
+    private string _connectionString;
+    public TestDbContext(string? connectionString = null)
+    {
+        _connectionString = connectionString
+            ?? "Server=localhost;Database=EFCoreLargeData;user id=sa;password=P@ssw0rd.123!;TrustServerCertificate=True";
+    }
     public DbSet<TextTable1MB> TextTable1MBs { get; set; }
     public DbSet<TextTable2MB> TextTable2MBs { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=localhost;Database=EFCoreLargeData;user id=sa;password=P@ssw0rd.123!;TrustServerCertificate=True");
+        optionsBuilder.UseSqlServer(_connectionString);
     }
 }
 
