ceb377f8f30f8961639506aeb7a95ccfd6f97349
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-12-23 22:40:26 +0000

    add Class2 class of tests

---
+++ xUnitTestRunBasic/Class2.cs
@@ -0,0 +1,40 @@
+﻿using Xunit.Abstractions;
+
+namespace xUnitTestRunBasic;
+
+[Collection("GuidCollection")]
+public class Class2
+{
+    private readonly ITestOutputHelper _output;
+    private readonly Guid guid;
+
+    public Class2(ITestOutputHelper output, GuidFixture guidFixture)
+    {
+        guid = guidFixture.Guid;
+        _output = output;
+    }
+
+    [Fact]
+    public async Task Test1()
+    {
+        _output.WriteLine($"Test1: {guid}");
+
+        await Task.Delay(1000);
+    }
+
+    [Fact]
+    public async Task Test2()
+    {
+        _output.WriteLine($"Test2: {guid}");
+
+        await Task.Delay(2000);
+    }
+
+    [Fact]
+    public async Task Test3()
+    {
+        _output.WriteLine($"Test3: {guid}");
+
+        await Task.Delay(3000);
+    }
+}
